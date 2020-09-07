afac0d876407ea09f071b1c557b0ca7a6206dc1a
tristanlatr <tris.la.tr@gmail.com>
2020-09-07 12:40:37 -0400

    Use pandoc to convert DOCS md file

--- publish.sh
+++ publish.sh
@@ -3,9 +3,14 @@ rm -rf ./dist
 rm -fr ./build
 rm -fr ./wpscan_out_parse.egg-info
 
-python3 -m pdoc wpscan_out_parse --pdf --force > DOCUMENTATION.md
-git add DOCUMENTATION.md
+python3 -m pdoc wpscan_out_parse --pdf --force > pdf.md
+pandoc --metadata=title:"WPScan Out Parse Documentation" \
+           --toc --toc-depth=4 --from=markdown+abbreviations \
+           --pdf-engine=xelatex --variable=mainfont:"DejaVu Sans" \
+           --output=docs.md --to=gfm pdf.md
+git add DOCS.md
 git commit -m "Generate docs"
+rm -f pdf.md
 git push
 
 python3 setup.py build check sdist bdist_wheel
@@ -15,3 +20,4 @@ python3 setup.py clean
 rm -rf ./dist
 rm -fr ./build
 rm -fr ./wpscan_out_parse.egg-info
+
